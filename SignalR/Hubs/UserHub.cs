ca7744a36c2ae507f9fd233c5d06d1f9f3308786
doaa <doaazakaria29@gmail.com>
2024-11-25 03:51:34 +0200

    Add method NewWindowLoaded

--- SignalR/Hubs/UserHub.cs
+++ SignalR/Hubs/UserHub.cs
@@ -4,5 +4,13 @@ namespace SignalR.Hubs
 {
     public class UserHub : Hub
     {
+        public static int TotalViews { get; set; } = 0;
+        public async Task NewWindowLoaded()
+        {
+            TotalViews++;
+            // send update to all conected clients that TotalViews has been updated
+            await Clients.All.SendAsync("updateTotalViews",TotalViews);
+       
+        }
     }
 }
